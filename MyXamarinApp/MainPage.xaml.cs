c3158a72d6c02fb5bdcf533e17c6e985dc653fd8
Arulraj A <arul.raj47@gmail.com>
2020-12-22 18:39:36 +0530

    Committed the example project.

---
+++ MyXamarinApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyXamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //dataGrid.AllowSorting = true;
+            //dataGrid.AllowTriStateSorting = true;
+            //dataGrid.AllowMultiSorting = true;
+            //dataGrid.SortTapAction = Syncfusion.SfDataGrid.XForms.SortTapAction.DoubleTap;
+            //dataGrid.SortColumnDescriptions.Add(new Syncfusion.SfDataGrid.XForms.SortColumnDescription()
+            //{
+            //    ColumnName = "OrderID",
+            //    SortDirection = Syncfusion.Data.ListSortDirection.Descending
+            //});
+
+            //dataGrid.SortColumnsChanging += DataGrid_SortColumnsChanging;
+        }
+
+        private void DataGrid_SortColumnsChanging(object sender, Syncfusion.SfDataGrid.XForms.DataGridSortColumnsChangingEventArgs e)
+        {
+            if(e.AddedItems[0].ColumnName == "OrderID")
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
